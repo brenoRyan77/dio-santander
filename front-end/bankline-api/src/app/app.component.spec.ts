b830e878855c0bede01450f44cfa0e24cd6a942a
bryan <bryan@indracompany.com>
2022-05-04 13:00:40 -0300

    fix: recriando ambiente Front-End

--- front-end/bankline-api/src/app/app.component.spec.ts
+++ front-end/bankline-api/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bankline-app'`, () => {
+  it(`should have as title 'bankline-api'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bankline-app');
+    expect(app.title).toEqual('bankline-api');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bankline-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bankline-api app is running!');
   });
 });
